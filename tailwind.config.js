037c58702d2c7da94d0f1e3a05ba2226bfada490
raziq99 <bagus061227@gmail.com>
2024-07-30 16:26:54 +0700

    upload pertama

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        navbar: "#69BCB8",
+        nav_hover: "#336F6C",
+        button_hover: "#468585",
+      },
+    },
+  },
+  plugins: [],
+};
